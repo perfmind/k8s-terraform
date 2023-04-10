7a247ea2b2dd2e799776a0c59f5b4671c9d08905
M. Sulton Hasanuddin <hi.sulton@gmail.com>
2023-04-10 18:32:15 +0700

    enable kms creation

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -1,28 +1,28 @@
-data "google_kms_key_ring" "cluster-keyring" {
+resource "google_kms_key_ring" "cluster-keyring" {
   project  = var.service_project
   name     = var.keyring_name
   location = var.kms_location # xxx: doesn't support zonal clusters
 }
 
-# resource "google_kms_crypto_key" "cluster-key" {
-#   name            = var.kms_name
-#   key_ring        = data.google_kms_key_ring.cluster-keyring.id
-#   rotation_period = var.rotation_period
-#   purpose         = var.purpose
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+resource "google_kms_crypto_key" "cluster-key" {
+  name            = var.kms_name
+  key_ring        = google_kms_key_ring.cluster-keyring.id
+  rotation_period = var.rotation_period
+  purpose         = var.purpose
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-#data "google_project" "service_project" {
-#  project_id = var.service_project
-#}
+data "google_project" "service_project" {
+  project_id = var.service_project
+}
 
-#resource "google_kms_crypto_key_iam_binding" "crypto_key" {
-#  crypto_key_id = google_kms_crypto_key.cluster-key.id
-#  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+resource "google_kms_crypto_key_iam_binding" "crypto_key" {
+  crypto_key_id = google_kms_crypto_key.cluster-key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
 
-#  members = [
-#    "serviceAccount:service-${data.google_project.service_project.number}@container-engine-robot.iam.gserviceaccount.com"
-#  ]
-#}
+  members = [
+    "serviceAccount:service-${data.google_project.service_project.number}@container-engine-robot.iam.gserviceaccount.com"
+  ]
+}
